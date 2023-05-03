dfda7058cfacc6e559c87a2dffd3efb31c0bcb3e
zyunhuku kou <ekqoql6418@yahoo.co.jp>
2023-05-03 21:38:03 +0900

    shippingPrice

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 /* . . . */
@@ -7,6 +8,10 @@ import { Injectable } from '@angular/core';
 
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 /* constructor() { } */
 
 addToCart(product: Product) {
@@ -20,6 +25,10 @@ getItems() {
 clearCart() {
   this.items = [];
   return this.items;
+}
+
+getShippingPrices() {
+  return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
 }
 /* . . . */
 }
